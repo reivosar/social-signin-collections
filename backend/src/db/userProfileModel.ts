0ffb0c9050acc3aa159211d7de6e77265ab85af4
reivosar <{ID}+{username}@users.noreply.github.com>
2024-03-05 03:13:28 +0900

    Refactored overall and completed DB registration implementation.

---
+++ backend/src/db/userProfileModel.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userProfileSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  name: String,
+});
+
+const UserProfile = mongoose.model("UserProfile", userProfileSchema);
+
+export default UserProfile;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+if (!process.env.MONGO_DB_URI) {
+  throw new Error("MONGODB_URI is not defined in the environment variables.");
+}
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    dbName: process.env.MONGO_DB_NAME,
+    user: process.env.MONGO_DB_USER,
+    pass: process.env.MONGO_DB_PASS,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(cors());
 app.use(express.json());
 app.use(authRoutes);
 
